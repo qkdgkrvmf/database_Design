1fa0545aae7c288947f8546c97999619ffa97317
jeongseolhwa <tjfghkwjd13@naver.com>
2024-11-20 21:58:20 +0900

    DB 연결

---
+++ Database_Design_back/src/test/java/com/database_Design/Database_Design/DatabaseDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database_Design.Database_Design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
